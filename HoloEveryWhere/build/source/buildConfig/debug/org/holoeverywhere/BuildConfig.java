1e13b15bd189a2425dde8ded84fa19756cb43428
TheFinestArtist <contact@thefinestartist.com>
2013-10-02 17:52:31 +0900

    Added Stuff

---
+++ HoloEveryWhere/build/source/buildConfig/debug/org/holoeverywhere/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.holoeverywhere;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
